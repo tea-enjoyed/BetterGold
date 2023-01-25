20b0db64be57b622d849479e48f9b50ecc8d5290
tea-enjoyed <yurykup0927@gmail.com>
2023-01-25 13:06:37 -0500

    b

--- src/main/java/net/tea_enjoyed/bettergold/item/ModItems.java
+++ src/main/java/net/tea_enjoyed/bettergold/item/ModItems.java
@@ -1,4 +1,20 @@
 package net.tea_enjoyed.bettergold.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.tea_enjoyed.bettergold.BetterGold;
+
+import java.rmi.registry.Registry;
+
 public class ModItems {
+    private static Item registerItem(String name, Item item){
+        return Registry.register(net.minecraft.util.registry.Registry.ITEM, new Identifier(BetterGold.MOD_ID, name), item);
+    }
+    public static final Item GOLDEN_SWORD = registerItem("golden_sword", new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item GOLDEN_AXE = registerItem("golden_axe", new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static void registerModItems() {
+        BetterGold.LOGGER.info("Registering mod items for Better Gold...");
+
+    }
 }
